b9f3f3af670d867c52291e343d1d6eadc2a65981
yagnamodi22 <yagnamodi22@gmail.com>
2025-10-29 16:22:32 +0530

    post

--- src/pages/TurfOwnerDashboard.jsx
+++ src/pages/TurfOwnerDashboard.jsx
@@ -717,7 +717,7 @@ const TurfOwnerDashboard = () => {
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Contact Info</th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Payment</th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
+                    
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
